6c58eeee8efc0fad51537ae33ec31f0d3e14d27a
basnetaakash23 <aakash.basnet@bison.howard.edu>
2022-08-07 16:58:35 -0500

    "Added Services to record todo."

--- src/main/java/com/bezkoder/spring/jwt/mongodb/services/TodoServiceImplementation.java
+++ src/main/java/com/bezkoder/spring/jwt/mongodb/services/TodoServiceImplementation.java
@@ -13,7 +13,6 @@ import java.util.stream.Collectors;
 
 @Service
 public class TodoServiceImplementation implements TodoService{
-    ObjectMapper objMapper = new ObjectMapper();
 
     @Autowired
     private ToDoRepository toDoRepository;
